351ad78b064eafd8ae52f677b9a0fe69b88c9351
Amirul Islam <35567854+shiningflash@users.noreply.github.com>
2019-11-10 13:04:04 +0600

    Update Flood_Fill_Algorithm.cpp

--- Flood_Fill_Algorithm.cpp
+++ Flood_Fill_Algorithm.cpp
@@ -17,16 +17,17 @@ int row, column;
 int dx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
 int dy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
 
-void floodfill(int i, int j) {
+void floodfill(int i, int j, int cnt) {
     if (i < 0 || j < 0 || i >= row || j >= column) return; // base case
 
     if (grid[i][j] == '.' && !vis[i][j]) {
         vis[i][j] = true;
+        grid[i][j] = (cnt + '0');
 
         for (int m = 0; m < 8; m++) {
             int x = i + dx[m];
             int y = j + dy[m];
-            floodfill(x, y);
+            floodfill(x, y, cnt);
         }        
     }
 }
@@ -52,15 +53,24 @@ int main() {
         for (int j = 0; j < column; j++) {
             if (grid[i][j] == '.' && !vis[i][j]) {
                 cnt++;
-                floodfill(i, j);
+                floodfill(i, j, cnt);
             }
         }
     }
-    printf("Number of dotted region: %d\n", cnt);
+    printf("Number of dotted region: %d\n\n", cnt);
+
+    // print new grid
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            printf("%c", grid[i][j]);
+        }
+        puts("");
+    }
 }
 
 /*
 Input:
+-----
 12 25
 @@@@@@@@@@@@@@@@@@@@@@@@@
 @@@@@@@@@@@@@@@@@@@@@@@@@
@@ -76,5 +86,20 @@ Input:
 @@@@@@@@@.....@@@@@@@@@@@
 
 Output:
+------
 Number of dotted region: 5
+
+@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@*************@@@@@@
+@@@@@@*1111*     22@@@@@@
+@@@@@@*1111*   2222@@@@@@
+@3@@@@*1111*      2@@@@@@
+@@@@@@*1111********@@44@@
+@@@@@@*11111111111*@@@444
+@@@@@@*11111111111*@@@44@
+@@@@@@*************@@@@4@
+@@@@@@@@@55555@@@@@@@@@@@
+@@@@@@@@@55555@@@@@@@@@@@
+
 */
